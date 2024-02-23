38d397391a7050a69c93b65e684bb1faa0e19543
akash-soni <akash.200287@gmail.com>
2024-02-23 12:35:57 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Akash",
+    author_email="akash.200287@gmail.com",
+    packages=find_packages()
+)
