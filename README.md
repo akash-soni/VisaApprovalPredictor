1ff9c316b104d5e6f3825b18f434b0cd4d2744ea
akash-soni <akash.200287@gmail.com>
2024-03-04 01:48:30 +0530

    data ingestion completed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # VisaApprovalPredictor
+
+
+
+# Workflow
+
+1. constant
+2. config_entity
+3. artifact_Entity
+4. component
+5. pipeline
+6. app.py
--- demo.py
+++ demo.py
@@ -1,11 +1,16 @@
 from us_visa.logger import logging
-from us_visa.exception import USViaException
+from us_visa.exception import USvisaException
+from us_visa.constants import *
 import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
 #logging.info("Welcome to our custom log")
 
-try:
-    a = 1 / "10"
-except Exception as e:
-    raise USViaException(e, sys) from e
+# try:
+#     a = 1 / "10"
+# except Exception as e:
+#     raise USViaException(e, sys) from e
 
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
